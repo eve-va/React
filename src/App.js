5a21c32d6ccf93a7bb724d39b53bb56745567289
eve-va <65899450+eve-va@users.noreply.github.com>
2021-01-14 17:14:02 +0200

    Components 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-//import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
 }
