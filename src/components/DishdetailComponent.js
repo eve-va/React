e6e907a2ba47a82caf61f7ea9e98a3770793bcd9
eve-va <65899450+eve-va@users.noreply.github.com>
2021-01-16 14:31:16 +0200

    Presentational and Container Components, Functional Components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+function RenderDish ({dish}) {
+    if (dish != null)
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>          
+        ); 
+    else 
+        return (
+            <div></div>
+        ); 
+}
+
+function RenderComments ({dish}) {
+    const dateParams = { year: 'numeric', month: 'short', day: '2-digit'};
+
+    if (dish != null)
+        return (
+            <div>
+                <h4>Comments</h4>
+                {dish.comments.map((comment) => {
+                    return (
+                    <div className="list-unstyled">
+                        <p>{comment.comment}</p>
+                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', dateParams).format(new Date(Date.parse(comment.date)))}</p>
+                    </div>
+                    );
+                })}
+            </div>
+        );     
+    else 
+        return (
+            <div></div>
+        );     
+}
+
+const DishDetail = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div  className="col-12 col-md-5 m-1">
+                    <RenderComments dish={props.dish} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DishDetail;
